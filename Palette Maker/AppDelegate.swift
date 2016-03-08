3a3275429e0dc73da3899af0c254355b5c0e1878
jesse-calkin <jesse.calkin@gmail.com>
2016-03-08 16:30:00 -0700

    Initial Commit

---
+++ Palette Maker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Palette Maker
+//
+//  Created by jesse calkin on 3/8/16.
+//  Copyright © 2016 Shoshin Boogie. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
